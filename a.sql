baabcb50d376441a768e5538b598282c353bfeed
jecastrom <jcastro.de@outlook.com>
2022-01-20 00:20:16 +0100

    a

--- a.sql
+++ a.sql
@@ -1,16 +1,16 @@
 SELECT
-    s.store_id,
-    c.city,
-    co.country,
-    sum(p.amount) AS total_sales
+    c.`name` AS film_category,
+    CONCAT(
+        FLOOR(ROUND((AVG(length))) / 60),
+        'h ',
+        MOD(ROUND((AVG(length))), 60),
+        'm'
+    ) AS average_movie_duration
 FROM
-    store s
-    INNER JOIN address a ON s.address_id = a.address_id
-    INNER JOIN city c ON a.city_id = c.city_id
-    INNER JOIN country co ON c.country_id = co.country_id
-    INNER JOIN customer cu ON s.store_id = cu.store_id
-    INNER JOIN payment p ON cu.customer_id = p.customer_id
+    film f
+    INNER JOIN film_category fc ON f.film_id = fc.film_id
+    INNER JOIN category c ON fc.category_id = c.category_id
 GROUP BY
     1
 ORDER BY
-    1;
+    2 DESC;
