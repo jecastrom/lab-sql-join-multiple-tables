7ebc72dd8a80e79632b1c4c80c02130dd8e81c28
jecastrom <jcastro.de@outlook.com>
2022-01-20 02:52:50 +0100

    a

--- a.sql
+++ a.sql
@@ -1,16 +1,14 @@
 SELECT
-    `name` AS film_category,
-    sum(amount) AS total_revenue
+    f.title AS film_title,
+    i.store_id AS availability_in_store,
+    count(f.title) AS number_of_films_available
 FROM
-    category c
-    INNER JOIN film_category fc ON c.category_id = fc.category_id
-    INNER JOIN film f ON f.film_id = fc.film_id
-    INNER JOIN inventory i ON f.film_id = i.film_id
-    INNER JOIN rental r ON i.inventory_id = r.inventory_id
-    INNER JOIN payment p ON r.rental_id = p.rental_id
+    inventory i
+    INNER JOIN film f ON i.film_id = f.film_id
+WHERE
+    title = 'Academy Dinosaur'
+    AND i.store_id = 1
 GROUP BY
     1
 ORDER BY
-    2 DESC
-LIMIT
-    5;
+    1;
