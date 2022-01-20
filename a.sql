eb28c8aa7006aa0217320d393a901ef3898e06fb
jecastrom <jcastro.de@outlook.com>
2022-01-20 02:16:26 +0100

    a

--- a.sql
+++ a.sql
@@ -1,13 +1,16 @@
 SELECT
-    f.title AS film_title,
-    count(r.inventory_id) AS rental_frequency
+    `name` AS film_category,
+    sum(amount) AS total_revenue
 FROM
-    film f
+    category c
+    INNER JOIN film_category fc ON c.category_id = fc.category_id
+    INNER JOIN film f ON f.film_id = fc.film_id
     INNER JOIN inventory i ON f.film_id = i.film_id
     INNER JOIN rental r ON i.inventory_id = r.inventory_id
+    INNER JOIN payment p ON r.rental_id = p.rental_id
 GROUP BY
     1
 ORDER BY
     2 DESC
 LIMIT
-    10;
+    5;
